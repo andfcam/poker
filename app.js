8af15b22ff7b8f920fff6667f84225d239bcc06b
Andy Cameron <Andy.Cameron@Nology.local>
2020-02-17 16:32:25 +0000

    Created Deck and Card classes

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 window.onload = () => {
     
 }
+
+// Create DOM space for cards
+// Create deck of cards
+// Shuffle deck of cards
+// Assign 2 cards to each player
+// Assign 5 random cards to the table
