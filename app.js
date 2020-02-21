af9b9ca15b5b3de8f4972de22474bf588c0cb5a9
Andy Cameron <Andy.Cameron@Nology.local>
2020-02-21 13:44:43 +0000

    Placeholder for required push. Ignore logic.js.

--- app.js
+++ app.js
@@ -13,13 +13,6 @@ window.onload = () => {
 // Raise:
 //      Take amount from slider when active
 
-// Keep track of last raise (dealer as default)
-//      Turn switches until all players (inc. big blind) have folded or called
-
-// Player has turn function that starts timer, allows actions etc.
-//      Comp and Player share same class and functions, just prompt.
-//      Player has 15 second timer, comp make decision in random time of 2 - 5s.
-
 // Split-pot can use bet() function to take the allotted amount.
 
 
--- logic.js
+++ logic.js
@@ -1,23 +1,45 @@
-const rankings = [
-    'royalFlush',
-    'straightFlush',
-    'fourOfAKind',
-    'fullHouse',
-    'flush',
-    'straight',
-    'threeOfAKind',
-    'twoPair',
-    'pair',
-    'highCard',
+const combinations = [
+    { name: 'royalFlush', required: 5 },
+    { name: 'straightFlush', required: 5 },
+    { name: 'fourOfAKind', required: 4 },
+    { name: 'fullHouse', required: 5 },
+    { name: 'flush', required: 5 },
+    { name: 'straight', required: 5 },
+    { name: 'threeOfAKind', required: 3 },
+    { name: 'twoPair', required: 4 },
+    { name: 'pair', required: 2 },
+    { name: 'highCard', required: 1 }
 ];
 
-const expected = { players: [ {name: '', cards: [Card, Card] }], table: { cards: [Card, Card, Card, Card, Card] } };
+const expected = players, table;
 
-data.players.forEach(player => {
-    calculateHandStrength(player);
+players.forEach(player => {
+    const cards = combineCards(player, table);
+    const combinations = calculateHandStrength(cards);
 });
 
+const combineCards = (player, table) => {
+    return table.cards.concat(player.table.cards);
+}
 
+const calculateHandStrength = (cards) => {
+    let combinations = [];
+    while (cards.length > 2) {
+        combinations.forEach(combination => {
+            if (cards.length - 2 >= combination.required) {
+                combinations.push(window[combination.name]());
+                // combination value could be reverse of combinations list index instead of explicity stating
+            }
+        });
+    }
+    return combinations;
+}
+
+
+
+// while there's more than 2 cards :
+//      check all combinations requirements
+//      push value to array of values [12, 6, 4, 1]
 
 // want to take in all cards
 // return [ Player, Player ] - list of winning players of length 1 or more
